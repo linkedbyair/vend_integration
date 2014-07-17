24271666701d97e17e8d00c1602aca9da5f1a004
Reinaldo Braga <reinaldob@gmail.com>
2014-07-17 16:37:04 -0300

    IN-331 IN-339 Add specs for vend integration

--- spec/support/parameters_factory.rb
+++ spec/support/parameters_factory.rb
@@ -1,9 +1,9 @@
 module Factories
   def self.parameters
     {
-      'vend_site_id'  => '123',
-      'vend_user'     => '123',
-      'vend_password' => '2',
+      'vend_site_id'  => 'spree',
+      'vend_user'     => 'spree@spreecommerce.com',
+      'vend_password' => '123',
     }
   end
 end
--- spec/vend_endpoint_spec.rb
+++ spec/vend_endpoint_spec.rb
@@ -13,41 +13,91 @@ describe VendEndpoint do
   let(:original) { Factories.original }
   let(:params) { Factories.parameters }
 
+  describe '/get_products' do
+    context 'success' do
+      it 'retrive products' do
+        message = {
+          request_id: '123456',
+          parameters: params
+        }.to_json
+
+        VCR.use_cassette('get_products') do
+          post '/get_products', message, auth
+          last_response.status.should == 200
+          last_response.body.should match /products were retrieved/
+        end
+      end
+    end
+  end
+
+  describe '/get_inventory' do
+    context 'success' do
+      it 'retrive inventory' do
+        message = {
+          request_id: '123456',
+          parameters: params
+        }.to_json
+
+        VCR.use_cassette('get_inventory') do
+          post '/get_inventory', message, auth
+          last_response.status.should == 200
+          last_response.body.should match /inventories were retrieved/
+        end
+      end
+    end
+  end
+
+  describe '/get_customers' do
+    context 'success' do
+      it 'retrive customers' do
+        message = {
+          request_id: '123456',
+          parameters: params
+        }.to_json
+
+        VCR.use_cassette('get_customers') do
+          post '/get_customers', message, auth
+          last_response.status.should == 200
+          last_response.body.should match /customers were retrieved/
+        end
+      end
+    end
+  end
+
+  describe '/get_orders' do
+    context 'success' do
+      it 'retrive orders' do
+        message = {
+          request_id: '123456',
+          parameters: params
+        }.to_json
+
+        VCR.use_cassette('get_orders') do
+          post '/get_orders', message, auth
+          last_response.status.should == 200
+          last_response.body.should match /orders were retrieved/
+        end
+      end
+    end
+  end
+
   describe '/add_order' do
     context 'success' do
-      it 'imports new orders' do
+      xit 'imports new orders' do
         message = {
           request_id: '123456',
           order: order,
           parameters: params
         }.to_json
 
-        VCR.use_cassette('import_new_order') do
+        VCR.use_cassette('send_order') do
           post '/add_order', message, auth
           last_response.should == ""
           last_response.status.should == 200
-          last_response.body.should match /was sent to Jirafe/
+          last_response.body.should match /was sent to Vend/
         end
       end
     end
-
-    # context 'failure' do
-    #   it 'returns error details 'do
-    #     order = Factories.order.merge({ :number => nil })
-
-    #     message = {
-    #       request_id: '123456',
-    #       order: order,
-    #       parameters: params
-    #     }.to_json
-
-    #     VCR.use_cassette('import_order_fail') do
-    #       post '/add_order', message, auth
-    #       last_response.status.should == 500
-    #       last_response.body.should match /None is not of type/
-    #     end
-    #   end
-    # end
   end
 
 
