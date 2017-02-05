9c48e7cf7e7129f531cfcf6633c5985c588ca376
Joe Lind <joelind@gmail.com>
2017-02-05 05:34:26 -0500

    Add page size to poll client

--- lib/vend/poll_client.rb
+++ lib/vend/poll_client.rb
@@ -10,7 +10,7 @@ module Vend
 
       define_method "get_#{method}" do |options|
         since = options[:since]
-        options = { headers: headers, query: { after: since.to_i } }
+        options = { headers: headers, query: { after: since.to_i, page_size: 25 } }
         response = self.class.get("/2.0/#{path}", options)
         validate_response response
         response
